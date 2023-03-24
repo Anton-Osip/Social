d22fb8f73edbd12f005304eef734b12f766510d7
Anton-Osip <osipchika@gmail.com>
2023-03-24 17:52:34 +0300

    12. Уроки React JS (Разбивка на компоненты)

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Header() {
+	return (
+		<header className='header'>
+			<img className='header__img' src='https://cryptologos.cc/logos/aave-aave-logo.png' alt='Logo' />
+		</header>
+	)
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function Navbar() {
+	return (
+		<nav className='nav'>
+			<div>
+				<a href='#aaa'>Profile</a>{' '}
+			</div>
+			<div>
+				<a href='#aaa'>Messages</a>
+			</div>
+			<div>
+				<a href='#aaa'>News</a>
+			</div>
+			<div>
+				<a href='#aaa'>Music</a>
+			</div>
+		</nav>
+	)
+}
