d138192eb950d75161c330102d9206c67f78febf
Anton-Osip <osipchika@gmail.com>
2023-03-24 17:41:00 +0300

    11. Уроки React JS (соц. сеть, шаг 1, вёрстка, grid)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import './App.css';
-
+import React from 'react'
+import './App.css'
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      BLABLABLA
-    </div>);
+	return (
+		<div className='app-wrapper'>
+			<header className='header'>
+				<img className='header__img' src='https://cryptologos.cc/logos/aave-aave-logo.png' alt='Logo' />
+			</header>
+			<nav className='nav'>
+				<div>
+					<a href='#aaa'>Profile</a>{' '}
+				</div>
+				<div>
+					<a href='#aaa'>Messages</a>
+				</div>
+				<div>
+					<a href='#aaa'>News</a>
+				</div>
+				<div>
+					<a href='#aaa'>Music</a>
+				</div>
+			</nav>
+			<div className='content'>
+				<div>
+					<img
+						className='content__img'
+						src='https://assets-global.website-files.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image.jpeg'
+						alt=''
+					/>
+				</div>
+				<div>ava + description</div>
+				<div>
+					my posts
+					<div>New posts</div>
+					<div>
+						<div>post 1</div>
+						<div>post 2</div>
+						<div>post 3</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
