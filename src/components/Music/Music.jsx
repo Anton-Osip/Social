8193c98ff1dd73720b82b44d3548f723092840ca
Anton-Osip <osipchika@gmail.com>
2023-03-25 09:33:19 +0300

    19. Уроки React JS (route, browser-router, маршрутизация) - react курсы бесплатно

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './Music.module.css'
+
+export function Music() {
+	return <div>Music</div>
+}
---
+++ src/components/News/News.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './News.module.css'
+
+export function News() {
+	return <div>News</div>
+}
