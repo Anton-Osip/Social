bf26684d652c8e26a1ed09b25c7809da76f5b1ad
Anton-Osip <osipchika@gmail.com>
2023-05-03 16:42:27 +0300

    77 - React JS - redux-form field validation (валидация, ошибки). Осторожно Замыкание!

--- src/components/common/FormsControls/FormsControls.jsx
+++ src/components/common/FormsControls/FormsControls.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import styles from './FormsControls.module.css'
 
-
-export function Textarea({ input, meta, ...props }) {
+const FormControl = ({ input, meta, child, ...props }) => {
 	const hasError = meta.touched && meta.error
 	return (
 		<div className={styles.form_control_textArea + ' ' + (hasError ? styles.error : '')}>
-			<textarea {...input} {...props} />
+			{props.children}
 			{hasError && <span>{meta.error}</span>}
 		</div>
 	)
 }
 
-export function Input({ input, meta, ...props }) {
-	const hasError = meta.touched && meta.error
+export function Textarea(props) {
+	const { input, meta, child, ...restProps } = props
 	return (
-		<div className={hasError ? styles.error : ''}>
-			<input {...input} {...props} />
-			{hasError && <span>{meta.error}</span>}
-		</div>
+		<FormControl {...props}>
+			<textarea {...input} {...restProps} />
+		</FormControl>
+	)
+}
+
+export function Input(props) {
+	const { input, meta, child, ...restProps } = props
+
+	return (
+		<FormControl {...props}>
+			<input {...input} {...restProps} />
+		</FormControl>
 	)
 }
